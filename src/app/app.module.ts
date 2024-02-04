1b387109bb7e1d14504458854c0e66539fce00f9
Holger Stolzenberg <holger.stolzenberg@me.com>
2024-02-04 19:18:54 +0100

    Introduced toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {BrowserModule, provideClientHydration} from "@angular/platform-browser";
 import {I18nModule} from "./i18n.module";
+import {ToolbarComponent} from "./toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    ToolbarComponent,
     AppComponent
   ],
   imports: [
