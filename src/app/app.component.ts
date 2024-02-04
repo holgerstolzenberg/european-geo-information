57df1cbdc2ccf5491d7d33f4716eb946e044aa2f
Holger Stolzenberg <holger.stolzenberg@me.com>
2024-02-04 13:57:56 +0100

    Properly setup sass file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.sass'
 })
 export class AppComponent {
   // nothing here so far
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {I18nModule} from "./i18n.module";
-import {provideRouter, RouterOutlet} from "@angular/router";
-import {routes} from "./app.routes";
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent
   ],
-  providers: [provideRouter(routes)],
   imports: [
     I18nModule,
     BrowserModule,
-    RouterOutlet,
   ],
   bootstrap: [AppComponent]
 })
