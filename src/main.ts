e082d9f45bc9f2d93be18d677eb748c0fc3627af
Holger Stolzenberg <holger.stolzenberg@me.com>
2024-02-11 00:46:44 +0100

    Add angular environments

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import {AppModule} from "./app/app.module";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {enableProdMode} from "@angular/core";
+import {environment} from "./environments/environment";
+
+if (environment.prodMode) {
+  enableProdMode();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
