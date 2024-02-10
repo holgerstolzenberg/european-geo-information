cdbbc6325940f94d71dba7cd270cfc6639d2e55b
Holger Stolzenberg <holger.stolzenberg@me.com>
2024-02-10 14:12:32 +0100

    Switch to dark leaflet tile layer

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,6 +6,15 @@ const openStreetMaps = tileLayer(
   {attribution: 'Open Streetmap', detectRetina: true}
 );
 
+const darkMatterNoLabels = tileLayer(
+  'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
+  }
+);
+
 const berlinC = circleMarker(
   [52.5200, 13.4050],
   {radius: 8, weight: 2, color: '#2626b9'}
@@ -22,12 +31,12 @@ export class MapComponent {
   options: MapOptions = {
     zoom: 4,
     minZoom: 3,
-    maxZoom: 15,
+    maxZoom: 20,
     center: latLng(54.5260, 15.2551) // Center of europe
   }
 
   layers = [
-    openStreetMaps,
+    darkMatterNoLabels,
     capitols
   ];
 }
