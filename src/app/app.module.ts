358bc0560aa161694a24afe1bff10580ee1ce355
Holger Stolzenberg <holger.stolzenberg@me.com>
2024-03-04 00:21:23 +0100

    Introduce MapModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapComponent } from './map/map.component';
-import { MapService } from './map/map.service';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
 import { environment } from '../environments/environment';
@@ -17,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { I18nModule } from './i18n/i18n.module';
 import { NotificationsModule } from './notifications/notifications.module';
+import { MapModule } from './map/map.module';
 
 @NgModule({
   declarations: [HeaderComponent, ToolbarComponent, MapComponent, AppComponent, OptionPaneComponent],
@@ -24,17 +23,17 @@ import { NotificationsModule } from './notifications/notifications.module';
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatIcon,
+    MatButton,
+    MatSlideToggle,
     LoggerModule.forRoot({
       level: environment.prodMode ? NgxLoggerLevel.WARN : NgxLoggerLevel.DEBUG
     }),
     I18nModule,
     NotificationsModule,
-    LeafletModule,
-    MatIcon,
-    MatButton,
-    MatSlideToggle
+    MapModule
   ],
-  providers: [provideAnimationsAsync(), MapService],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
